396e17a449155f6bcfad1065098a1b6582ed4123
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-08 23:56:01 +0530

    Update volunteer.php

--- modules/pages/volunteer.php
+++ modules/pages/volunteer.php
@@ -2,76 +2,76 @@
     <div class="catagory_container ">
         <div class="container ">
             <div class="heading_container heading_center">
-                <h2>
+                <h6>
                    Our Volunteer's
-                </h2>
+                </h6>
                 <br>
                 <table class="table table-bordered">
 
                     <tbody>
                         <tr>
                             <th><img src="./members/Ramesh K.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Ramesh K</h2></td>
+                            <td ><h6>Ramesh K</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/Sriduar N.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Sridhar N</h2></td>
+                            <td ><h6>Sridhar N</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/Muninarayappa.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Muninarayanappa</h2></td>
+                            <td ><h6>Muninarayanappa</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/Chinnaswamy.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Chinnaswamy</h2></td>
+                            <td ><h6>Chinnaswamy</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/Mashesh kumar.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Mashesh kumar</h2></td>
+                            <td ><h6>Mashesh kumar</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/Govindappa.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Govindappa</h2></td>
+                            <td ><h6>Govindappa</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/Chalapathi.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Chalapathi</h2></td>
+                            <td ><h6>Chalapathi</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/Rajesh.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Rajesh</h2></td>
+                            <td ><h6>Rajesh</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/Nagarage.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Nagaraj</h2></td>
+                            <td ><h6>Nagaraj</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/Manjunatha.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Manjunatha V</h2></td>
+                            <td ><h6>Manjunatha V</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/M Denash kumar.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>M Denash kumar</h2></td>
+                            <td ><h6>M Denash kumar</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/Ravi.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Ravi</h2></td>
+                            <td ><h6>Ravi</h6></td>
 
                         </tr>
                         <tr>
                             <th><img src="./members/Pradeep.jpeg" width="150px" alt="" srcset=""> </th>
-                            <td ><h2>Pradeep S</h2></td>
+                            <td ><h6>Pradeep S</h6></td>
 
                         </tr>
                         
