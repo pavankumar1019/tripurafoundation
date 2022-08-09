25e1a196ade7ce7ff4dc761a8faf04b3595eb2e0
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-09 09:17:33 +0530

    a

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href="upi://pay?pa=UPIID@oksbi&amp;pn=FNAME SNAME K&amp;cu=INR" class="upi-pay1">Pay Now !</a>
+
+</body>
+</html>
