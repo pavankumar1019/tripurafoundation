cac45cddf85867700aff579772d02eca1fd0d721
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-09 09:30:19 +0530

    a

--- test.php
+++ test.php
@@ -8,6 +8,25 @@
 </head>
 <body>
 <a href="upi://pay?pa=UPIID@oksbi&amp;pn=FNAME SNAME K&amp;cu=INR" class="upi-pay1">Pay Now !</a>
-
+<?php
+         $to = "pavan5639kumar@gmail.com";
+         $subject = "This is subject";
+         
+         $message = "<b>This is HTML message.</b>";
+         $message .= "<h1>This is headline.</h1>";
+         
+         $header = "From:info@pkwebdev.com \r\n";
+         $header .= "Cc:info@pkwebdev.com \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+      ?>
 </body>
 </html>
