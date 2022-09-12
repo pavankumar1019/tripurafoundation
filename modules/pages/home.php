0e59f4f5ebcee60a5c69285af5f20219ca77d4a0
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-09-12 20:37:29 +0530

    a

--- modules/pages/home.php
+++ modules/pages/home.php
@@ -315,7 +315,12 @@
         </div>
         <div class="row">
             <div class="col-md-6 mx-auto">
-                
+            <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
             </div>
         </div>
     </div>
